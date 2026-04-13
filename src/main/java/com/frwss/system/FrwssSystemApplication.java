391e7a90a8414fdcd3d9113db4e05ebcfc8ae20b
Prabuddha Bandara <ptb25803@gmail.com>
2026-04-13 09:59:46 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/frwss/system/FrwssSystemApplication.java
@@ -0,0 +1,15 @@
+package com.frwss.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FrwssSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrwssSystemApplication.class, args);
+    }
+
+}
