c8231afd39d61c9896d0cd562e4f9f7d89ea9ff7
Prabuddha <ptb25803@gmail.com>
2026-04-13 11:53:25 +0530

    Added file upload UI page

--- src/main/java/com/frwss/system/controller/IngestionController.java
+++ src/main/java/com/frwss/system/controller/IngestionController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IngestionController {
+
     @GetMapping("/ingestion")
     public String ingestionPage(){
         return "ingestion/dashboard";
+    }
+
+    @GetMapping("/ingestion/upload")
+    public String uploadPage(){
+        return "ingestion/upload";
     }
 }
