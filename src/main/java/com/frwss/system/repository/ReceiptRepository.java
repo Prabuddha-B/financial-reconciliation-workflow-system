7319415b71de662c763ba91305606609c95b77e8
Prabuddha <ptb25803@gmail.com>
2026-04-18 23:27:38 +0530

    Integrate ingestion module with backend and DB connection; merge manual entry UI (pending template fix)

--- src/main/java/com/frwss/system/repository/ReceiptRepository.java
+++ src/main/java/com/frwss/system/repository/ReceiptRepository.java
@@ -4,7 +4,10 @@ import com.frwss.system.model.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, String> {
     boolean existsByReferenceNo(String referenceNo);
+    Optional<Receipt> findByReferenceNo(String referenceNo);
 }
